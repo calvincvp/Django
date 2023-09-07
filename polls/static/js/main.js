322b4cef8efb29d44eb516e192c8cc6d654b2fa4
calvincvp <72568420+calvincvp@users.noreply.github.com>
2023-09-07 09:24:27 +0530

    Add files via upload

---
+++ polls/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+
+    })
+
+    $('.owl-carousel').owlCarousel();
+
+});
+
